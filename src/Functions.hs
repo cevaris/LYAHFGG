3ccff307cd056e77f7edff6a594338f2ba466638
Adam Cardenas <cevaris@gmail.com>
2014-10-24 12:02:06 -0600

    got working general data type for midpoint calc.

--- src/Functions.hs
+++ src/Functions.hs
@@ -13,15 +13,17 @@ sayMe x = "Not between 1 and 5"
 
 
 --------------------------------------------------------
--- Point Manipulation
---data Point =  Point2 Integer Integer 
---            | Point3 Integer Integer Integer
+--Point Manipulation
 
-type Point = (Integer, Integer)
+--type Point = (Integer, Integer)
 
---midpoint :: (Point a) => a -> a -> a
---midpoint (Point2 x1 y1) (Point2 x2 y2)       = (Point2 2 2)
---midpoint (Point3 x1 y1 z1) (Point3 x2 y2 z2) = (Point3 2 2 3)
+--midpoint :: Point -> Point -> Point
+--midpoint (x1,y1) (x2, y2) = (((x1+x2) `div` 2), ((y1+y2) `div` 2))
+
+
+data Point =  Point2 Integer Integer 
+            | Point3 Integer Integer Integer deriving (Show, Eq)
 
 midpoint :: Point -> Point -> Point
-midpoint (x1,y1) (x2, y2) = (((x1+x2) `div` 2), ((y1+y2) `div` 2))
+midpoint (Point2 x1 y1) (Point2 x2 y2) = (Point2 ((x1+x2) `div` 2) ((y1+y2) `div` 2))
+midpoint (Point3 x1 y1 z1) (Point3 x2 y2 z2) = (Point3 z1 y1 x1)
