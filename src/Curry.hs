3504fca3cda40d32c5adbdea1088689d176dcc42
Adam Cardenas <cevaris@gmail.com>
2014-10-27 13:49:11 -0600

    working on curry

--- src/Curry.hs
+++ src/Curry.hs
@@ -1,3 +1,20 @@
 module Curry where
 
 
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering  
+compareWithHundred = compare 100 
+
+dupFunc :: (a -> a) -> a -> a
+dupFunc f x = f $ f x
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _  = []
+zipWith' _ _  [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+largestDisvisiable :: (Integral a) => a -> a
+largestDisvisiable n = head (filter p [n,n-1..])
+  where p x = x `mod` 3829 == 0
